d440522f4e7ba801518fc091d75650865f8feccd
devnewton <devnewton@bci.im>
2016-10-02 20:18:15 +0200

    use pseudo behavior tree for Grobelin

--- app/entities/Grobelin.ts
+++ app/entities/Grobelin.ts
@@ -10,7 +10,7 @@ export class Grobelin extends Phaser.Sprite {
     currentPathPointTarget: Phaser.Point;
     attack = false;
     thinking = false;
-    private pathfinder: Pathfinder;    
+    private pathfinder: Pathfinder;
 
     constructor(game: Phaser.Game, pathfinder: Pathfinder) {
         super(game, 0, 0, 'grobelin');
@@ -52,34 +52,108 @@ export class Grobelin extends Phaser.Sprite {
     update() {
         super.update();
         if (this.exists) {
-            this.attackIfNearTarget();
-            this.walk();
-            this.think();
+            this.executeBehaviorTree();
             this.animate();
         }
     }
 
-    walk() {
-        if (this.attack) {
-            this.body.velocity.x = 0;
-            this.body.velocity.y = 0;
-        } else {
-            if (this.currentPathPointTarget) {
-                this.moveToXY(this.currentPathPointTarget.x, this.currentPathPointTarget.y, 300);
-                if (Phaser.Math.distance(this.body.center.x, this.body.center.y, this.currentPathPointTarget.x, this.currentPathPointTarget.y) < this.body.halfWidth) {
-                    this.currentPathPointTarget = null;
-                }
+    private executeBehaviorTree() {
+        this.priority_Root();
+    }
+
+    private priority_Root() {
+        this.sequence_Attack() || this.action_FollowPath() || this.action_SearchAPathToEnemy();
+    }
+
+    private sequence_Attack() {
+        return this.condition_IsNearEnemy() && this.action_AttackEnemy();
+    }
+
+    private condition_IsNearEnemy(): boolean {
+        return this.attack = this.enemy && Phaser.Math.distance(this.body.center.x, this.body.center.y, this.enemy.body.center.x, this.enemy.body.center.y) < this.body.width * 2;
+    }
+
+    private action_AttackEnemy(): boolean {
+        this.attack = true;
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
+        const dx = this.enemy.body.center.x - this.body.center.x;
+        const dy = this.enemy.body.center.y - this.body.center.y;
+        if (Math.abs(dx) > Math.abs(dy)) {
+            if (dx < 0) {
+                this.play("lpc.thrust.left", 8, false);
+            } else if (dx > 0) {
+                this.play("lpc.thrust.right", 8, false);
+            } else if (dy < 0) {
+                this.play("lpc.thrust.back", 8, false);
+            } else if (dy > 0) {
+                this.play("lpc.thrust.front", 8, false);
             } else {
-                this.currentPathPointTarget = this.path.shift();
-                if (this.path.length == 0) {
-                    this.body.velocity.x = 0;
-                    this.body.velocity.y = 0;
-                }
+                this.play("lpc.thrust.right", 8, false);
+            }
+        } else {
+            if (dy < 0) {
+                this.play("lpc.thrust.back", 8, false);
+            } else if (dy > 0) {
+                this.play("lpc.thrust.front", 8, false);
+            } else if (dx < 0) {
+                this.play("lpc.thrust.left", 8, false);
+            } else if (dx > 0) {
+                this.play("lpc.thrust.right", 8, false);
+            } else {
+                this.play("lpc.thrust.back", 8, false);
+            }
+        }
+        return true;
+    }
+
+    private action_FollowPath(): boolean {
+        if (this.currentPathPointTarget) {
+            this.action_FollowPath_MoveToXY(this.currentPathPointTarget.x, this.currentPathPointTarget.y, 300);
+            if (Phaser.Math.distance(this.body.center.x, this.body.center.y, this.currentPathPointTarget.x, this.currentPathPointTarget.y) < this.body.halfWidth) {
+                this.currentPathPointTarget = null;
+            }
+        } else {
+            this.currentPathPointTarget = this.path.shift();
+            if (this.path.length == 0) {
+                this.body.velocity.x = 0;
+                this.body.velocity.y = 0;
+                return false;
+            }
+        }
+        this.action_FollowPath_Animate();
+        return true;
+    }
+
+    action_FollowPath_Animate() {
+        if (Math.abs(this.body.velocity.x) > Math.abs(this.body.velocity.y)) {
+            if (this.body.velocity.x < 0) {
+                this.play("lpc.walk.left", 8, false);
+            } else if (this.body.velocity.x > 0) {
+                this.play("lpc.walk.right", 8, false);
+            } else if (this.body.velocity.y < 0) {
+                this.play("lpc.walk.back", 8, false);
+            } else if (this.body.velocity.y > 0) {
+                this.play("lpc.walk.front", 8, false);
+            } else {
+                this.play("lpc.hurt", 0, false);
+            }
+        } else {
+            if (this.body.velocity.y < 0) {
+                this.play("lpc.walk.back", 8, false);
+            } else if (this.body.velocity.y > 0) {
+                this.play("lpc.walk.front", 8, false);
+            } else if (this.body.velocity.x < 0) {
+                this.play("lpc.walk.left", 8, false);
+            } else if (this.body.velocity.x > 0) {
+                this.play("lpc.walk.right", 8, false);
+            } else {
+                this.play("lpc.hurt", 0, false);
             }
         }
     }
 
-    moveToXY(x: number, y: number, speed: number = 60) {
+    private action_FollowPath_MoveToXY(x: number, y: number, speed: number = 60) {
         var angle = Math.atan2(y - this.body.center.y, x - this.body.center.x);
         /*var distance = Phaser.Math.distance(this.body.center.x, this.body.center.y, x, y);
         speed = Math.max(distance, speed);*/
@@ -87,75 +161,22 @@ export class Grobelin extends Phaser.Sprite {
         this.body.velocity.y = Math.sin(angle) * speed;
     }
 
-    attackIfNearTarget() {
-        this.attack = this.enemy && Phaser.Math.distance(this.body.center.x, this.body.center.y, this.enemy.body.center.x, this.enemy.body.center.y) < this.body.width * 2;
-    }
-
-    think() {
-        if (!this.attack && !this.thinking && !this.currentPathPointTarget && this.path.length == 0) {
+    private action_SearchAPathToEnemy(): boolean {
+        if (!this.thinking) {
             this.thinking = true;
             this.pathfinder.findPath(this.body.center.x, this.body.center.y, this.enemy.body.center.x, this.enemy.body.center.y, (path: Phaser.Point[]) => {
                 this.path = path || new Array<Phaser.Point>();
                 this.thinking = false;
             });
         }
+        return this.path.length == 0;
     }
 
-    animate() {
+    private animate() {
         if (this.attack) {
-            const dx = this.enemy.body.center.x - this.body.center.x;
-            const dy = this.enemy.body.center.y - this.body.center.y;
-            if (Math.abs(dx) > Math.abs(dy)) {
-                if (dx < 0) {
-                    this.play("lpc.thrust.left", 8, false);
-                } else if (dx > 0) {
-                    this.play("lpc.thrust.right", 8, false);
-                } else if (dy < 0) {
-                    this.play("lpc.thrust.back", 8, false);
-                } else if (dy > 0) {
-                    this.play("lpc.thrust.front", 8, false);
-                } else {
-                    this.play("lpc.thrust.right", 8, false);
-                }
-            } else {
-                if (dy < 0) {
-                    this.play("lpc.thrust.back", 8, false);
-                } else if (dy > 0) {
-                    this.play("lpc.thrust.front", 8, false);
-                } else if (dx < 0) {
-                    this.play("lpc.thrust.left", 8, false);
-                } else if (dx > 0) {
-                    this.play("lpc.thrust.right", 8, false);
-                } else {
-                    this.play("lpc.thrust.back", 8, false);
-                }
-            }
+
         } else {
-            if (Math.abs(this.body.velocity.x) > Math.abs(this.body.velocity.y)) {
-                if (this.body.velocity.x < 0) {
-                    this.play("lpc.walk.left", 8, false);
-                } else if (this.body.velocity.x > 0) {
-                    this.play("lpc.walk.right", 8, false);
-                } else if (this.body.velocity.y < 0) {
-                    this.play("lpc.walk.back", 8, false);
-                } else if (this.body.velocity.y > 0) {
-                    this.play("lpc.walk.front", 8, false);
-                } else {
-                    this.play("lpc.hurt", 0, false);
-                }
-            } else {
-                if (this.body.velocity.y < 0) {
-                    this.play("lpc.walk.back", 8, false);
-                } else if (this.body.velocity.y > 0) {
-                    this.play("lpc.walk.front", 8, false);
-                } else if (this.body.velocity.x < 0) {
-                    this.play("lpc.walk.left", 8, false);
-                } else if (this.body.velocity.x > 0) {
-                    this.play("lpc.walk.right", 8, false);
-                } else {
-                    this.play("lpc.hurt", 0, false);
-                }
-            }
+
         }
     }
 }
